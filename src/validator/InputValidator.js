808c0d27e240bfe0e0d81ca180ee7ffa3cf5394b
ReturnReason <reasonz@daum.net>
2022-12-10 18:45:50 +0900

    feat(InputValidator): 사용자가 당첨 번호 입력시 콤마 외의 구분자를 사용했을 때 예외처리하는 메서드 추가

--- src/validator/InputValidator.js
+++ src/validator/InputValidator.js
@@ -2,10 +2,10 @@ const { LOTTO } = require('../constants/lotto');
 const { ERROR } = require('../constants/errorMessages');
 
 class InputValidator {
-  checkValidMoney(input) {
+  checkValidMoney(money) {
     const { amount } = LOTTO;
     const { invalidRangeMoney } = ERROR;
-    if (input % amount) {
+    if (money % amount) {
       throw invalidRangeMoney;
     }
     return true;
@@ -26,6 +26,28 @@ class InputValidator {
       throw emptyInput;
     }
     return true;
+  }
+
+  checkValidWinningNumbersSeparator(winningNumbers) {
+    const { invalidWinningNumbersSeparator } = ERROR;
+    const removeNumber = winningNumbers.replace(/[0-9]/gi, '');
+    if (removeNumber.split(',').join('').length) {
+      throw invalidWinningNumbersSeparator;
+    }
+    return true;
+  }
+
+  checkValidWinningNumbersLenght(winningNumber) {
+    // 로또 번호 6개 입력했는지 확인
+    // const { count } =LOTTO;
+    // const {invalidWinningNumbersLength } =ERROR;
+    // const lottoNumbers = winningNumber.split(',');
+    // if(lottoNumbers.length !== count) throw invalidWinningNumbersLength;
+    // lottoNumbers.forEach(())
+  }
+
+  checkValidLottoNumbersRange(lottoNumber) {
+    // 로또 번호가 1~45 사이 숫자인지 확인
   }
 }
 
