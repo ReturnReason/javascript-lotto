130d50c6f06c4c8b6893f831de2c88bce9cb6eac
ReturnReason <reasonz@daum.net>
2022-12-10 16:56:13 +0900

    refactor(LottoGameController): 구입 금액 예외처리하는 코드 메서드로 분리

--- src/controller/LottoGameController.js
+++ src/controller/LottoGameController.js
@@ -1,25 +1,39 @@
 const InputValidator = require('../validator/InputValidator');
 const InputView = require('../view/InputView');
 const OutputView = require('../view/OutputView');
+const Lotto = require('../Lotto');
+
 const { countLottoQuantity } = require('../utils/calculator');
+const LottoMachine = require('../model/LottoMachine');
+const User = require('../model/User');
 
 class LottoGameController {
   #inputView;
   #outputView;
+  #lottoMachine;
+  #user;
+  #lotto;
 
   constructor() {
     this.#inputView = new InputView();
     this.#outputView = new OutputView();
+    this.#lottoMachine = new LottoMachine();
+    this.#user = new User();
     this.inputValidator = new InputValidator();
   }
 
   start() {
     const userInput = (input) => {
-      this.inputValidator.checkValidMoney(input);
+      this.validate(input);
       const lottoQuantity = countLottoQuantity(input);
       this.#outputView.printLottoQuantity(lottoQuantity);
     };
     this.#inputView.readMoney(userInput);
+  }
+
+  validate(input) {
+    this.inputValidator.checkValidMoney(input);
+    this.inputValidator.checkValidInput(input);
   }
 }
 
