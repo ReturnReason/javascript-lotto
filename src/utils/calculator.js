e4dff4c33fd057446b3a25ad0b1fe6a8cc736edc
ReturnReason <reasonz@daum.net>
2022-12-11 21:07:27 +0900

    feat(calculator): 수익률을 계산하여 반환하는 유틸함수 구현

--- src/utils/calculator.js
+++ src/utils/calculator.js
@@ -1,11 +1,18 @@
-const { LOTTO } = require('../constants/lotto');
+const { LOTTO, PRIZE_MONEY } = require('../constants/lotto');
 
 const countLottoQuantity = (money) => {
   const { amount } = LOTTO;
   return money / amount;
 };
 
-const calculatorProfitRate = (profitRate) => {};
+const calculatorProfitRate = (totalCounter, money) => {
+  let result = 0;
+  Object.entries(totalCounter).forEach(([match, count]) => {
+    result += ((PRIZE_MONEY[match] * count) / money) * 100;
+  });
+
+  return result.toFixed(1).toLocaleString();
+};
 
 module.exports = {
   countLottoQuantity,
